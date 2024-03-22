2d226d98fe25ebfd4bbfb445049e3fd6b05ef24d
Radovan Tkac <tkac.radovan@gmail.com>
2024-03-22 12:39:20 +0100

    feat(all): 404 page update and biome check fix

--- apps/demo-app/src/app/pages/not-found/NotFound.tsx
+++ apps/demo-app/src/app/pages/not-found/NotFound.tsx
@@ -1,3 +1,16 @@
+import { useTranslation } from "react-i18next";
+
 export const NotFound: React.FC = () => {
-	return <div>404 Not Found</div>;
+	const { t } = useTranslation();
+
+	return (
+		<div className="w-full p-10">
+			<h2 className="mb-6 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
+				{t("404.heading")}
+			</h2>
+			<p className="text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400 text-center">
+				{t("404.description")}
+			</p>
+		</div>
+	);
 };
--- libs/ui/src/layouts/MainLayout.tsx
+++ libs/ui/src/layouts/MainLayout.tsx
@@ -8,7 +8,7 @@ export const MainLayout: React.FC<MainLayoutProps> = ({ coreHeader }) => {
 	return (
 		<div>
 			{coreHeader}
-			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-8 px-4">
 				<Outlet />
 			</div>
 		</div>
