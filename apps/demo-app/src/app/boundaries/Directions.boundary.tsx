a042c5eeb251497316c97711118b81bc43e589ae
Radovan Tkac <tkac.radovan@gmail.com>
2024-03-26 13:30:22 +0100

    feat(all): added spinner component, updated useDir hook

--- apps/demo-app/src/app/boundaries/Directions.boundary.tsx
+++ apps/demo-app/src/app/boundaries/Directions.boundary.tsx
@@ -1,17 +1,13 @@
+import { useDir } from "@core/hooks";
 import { DirectionProvider } from "@radix-ui/react-direction";
-import { type PropsWithChildren, useMemo } from "react";
-import { useTranslation } from "react-i18next";
+import type { PropsWithChildren } from "react";
 
 interface DirectionsBoundaryProps extends PropsWithChildren {}
 
 export const DirectionsBoundary: React.FC<DirectionsBoundaryProps> = ({
 	children,
 }) => {
-	const { i18n } = useTranslation();
-
-	const direction = useMemo(() => {
-		return i18n.dir(i18n.resolvedLanguage);
-	}, [i18n, i18n.resolvedLanguage]);
+	const { direction } = useDir();
 
 	return <DirectionProvider dir={direction}>{children}</DirectionProvider>;
 };
--- apps/demo-app/src/app/pages/home/Home.tsx
+++ apps/demo-app/src/app/pages/home/Home.tsx
@@ -4,14 +4,24 @@ export const Home: React.FC = () => {
 	const { t } = useTranslation();
 
 	return (
-		<div>
-			<p className="text-md font-normal text-gray-500 lg:text-lg">
-				{t("home.paragraph")}
-			</p>
-			<br />
-			<p className="text-md font-normal text-gray-500 lg:text-lg">
-				{t("home.paragraph")}
-			</p>
-		</div>
+		<>
+			<div>
+				<p className="text-md font-normal text-gray-500 lg:text-lg">
+					{t("home.paragraph")}
+				</p>
+				<br />
+				<p className="text-md font-normal text-gray-500 lg:text-lg">
+					{t("home.paragraph")}
+				</p>
+			</div>
+			<div className="w-full my-10">
+				<div className="grid md:grid-cols-4 sm:grid-cols-2 gap-4">
+					<div className="rounded-lg shadow-lg bg-violet-100 h-40" />
+					<div className="rounded-lg shadow-lg bg-violet-200 h-40" />
+					<div className="rounded-lg shadow-lg bg-violet-300 h-40" />
+					<div className="rounded-lg shadow-lg bg-violet-400 h-40" />
+				</div>
+			</div>
+		</>
 	);
 };
