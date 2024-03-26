43565c0666606035a8440a36f0dbcc33133e4a64
Radovan Tkac <tkac.radovan@gmail.com>
2024-03-26 10:13:53 +0100

    feat(all): update app entry component, add test command

--- apps/demo-app/src/app/app.spec.tsx
+++ apps/demo-app/src/app/app.spec.tsx
@@ -1,15 +1,10 @@
 import { render } from "@testing-library/react";
 
-import App from "./app";
+import AppEntry from "./AppEntry";
 
 describe("App", () => {
 	it("should render successfully", () => {
-		const { baseElement } = render(<App />);
+		const { baseElement } = render(<AppEntry />);
 		expect(baseElement).toBeTruthy();
-	});
-
-	it("should have a greeting as the title", () => {
-		const { getByText } = render(<App />);
-		expect(getByText(/Welcome demo-app/gi)).toBeTruthy();
 	});
 });
