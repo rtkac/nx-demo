4277dc45d28aba944c88078b5b646b287d644b0a
Radovan Tkac <tkac.radovan@gmail.com>
2024-03-26 14:48:03 +0100

    feat(demo-app): home page changes

--- apps/demo-app/src/app/pages/home/Home.tsx
+++ apps/demo-app/src/app/pages/home/Home.tsx
@@ -16,7 +16,7 @@ export const Home: React.FC = () => {
 			</div>
 			<div className="w-full my-10">
 				<div className="grid md:grid-cols-4 sm:grid-cols-2 gap-4">
-					<div className="rounded-lg shadow-lg bg-violet-100 h-40" />
+					<div className="rounded-lg shadow-lg bg-violet-100 h-40 rtl:bg-mauve12" />
 					<div className="rounded-lg shadow-lg bg-violet-200 h-40" />
 					<div className="rounded-lg shadow-lg bg-violet-300 h-40" />
 					<div className="rounded-lg shadow-lg bg-violet-400 h-40" />
